ae4509c10062c0d768e2b67c2eaa055fbf777e06
Joel Lehtonen <jlehtone@c1r5p14.hive.fi>
2024-09-20 13:55:18 +0300

    first

--- src/input_parser.c
+++ src/input_parser.c
@@ -6,7 +6,7 @@
 /*   By: jlehtone <jlehtone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 13:19:30 by jlehtone          #+#    #+#             */
-/*   Updated: 2024/09/20 13:52:10 by jlehtone         ###   ########.fr       */
+/*   Updated: 2024/09/20 13:53:16 by jlehtone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void input_parser(t_shell *shell)
 {
-	if (check_if_even_chars(shell) == false); // MAKE LATER. checks if there is an even amount of quotes, brackets, etc. Maybe can be done later too?
+	if (check_if_even_chars(shell) == false); // MAKE LATER. checks if there is an even amount of quotes, brackets, etc. OR Maybe can be done later too?
 		return ;
 }
