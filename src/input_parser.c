8cfcc335d15f2c15ca1737b162ecee6abc31eb8f
Joel Lehtonen <jlehtone@c1r5p14.hive.fi>
2024-09-23 16:52:52 +0300

    tokenizer progress

--- src/input_parser.c
+++ src/input_parser.c
@@ -6,7 +6,7 @@
 /*   By: jlehtone <jlehtone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 13:19:30 by jlehtone          #+#    #+#             */
-/*   Updated: 2024/09/23 14:37:59 by jlehtone         ###   ########.fr       */
+/*   Updated: 2024/09/23 16:52:08 by jlehtone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,38 @@ void input_error_check(t_shell *shell)
 	}
 	if (single_quotes % 2 != 0 || double_quotes % 2 != 0)
 		free_and_exit(); //error message?
+}
 
 void tokenize_input(t_shell *shell)
 {
-	int	index;
+	int		index;
+	int		token_index;
+	int		start;
+	int		end;
+	t_token *new;
 
 	index = 0;
 	input_error_check(shell);
-	while (shell->user_input)
+	shell->token_pointer = NULL;
+	while (shell->user_input[index] != '\0')
 	{
-		if (shell->user_input[index] == ' ') // add other white spaces as well, maybe define in the header
+		token_index = 0;
+		while (shell->user_input[index] == ' ') // add other white spaces as well, maybe define in the header
 			index++;
-		if (isalpha(shell->user_input[index]) == true)
-		
-		index++;
+		start = index;
+		while (isalpha(shell->user_input[index]) == true)
+			index++;
+		end = index;
+		new = ft_lstnew_token(NULL);
+		new->line = malloc(sizeof(char) * (end - start + 1));
+		if (new->line == NULL)
+			free_and_exit(shell); // DO THIS LATER
+		index = start;
+		while (isalpha(shell->user_input[index]) == true)
+			new->line[token_index++] = shell->user_input[index++];
+		new->line[token_index] = '\0';
+		ft_lstadd_back_token(&shell->token_pointer, new);
+		new->level++;
+		// if (isseparator(shell->user_input[index]) == true)
 	}
 }
