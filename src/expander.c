567e3eb63f96c153670740920bc902627214976e
Joel Lehtonen <jlehtone@c2r5p5.hive.fi>
2024-10-04 12:36:14 +0300

    working on expander

--- src/expander.c
+++ src/expander.c
@@ -2,12 +2,66 @@
 
 #include "../inc/minishell.h"
 
-void	expander(t_shell *shell)
-{
+void	replace_dollar(t_shell *shell, t_token *token)
+{	
+	int	index;
+	int	copy_index;
+	char *replacement;
+	int	single_quote;
+	int	double_quote;
+
+	index = 0;
+	copy_index = 0;
+	single_quote = false;
+	double_quote = false;
+	replacement = malloc(sizeof(char) * (ft_strlen(token->line) + 1));
+	if (!replacement)
+		error_printer(shell, MALLOC_FAIL, true);
+	while (token->line[index] != '\0')
+	{
+		if (token->line[index] == '\'' && double_quote == false)
+		{
+			single_quote = !single_quote;
+			index++;
+		}
+		if (token->line[index] == '\"' && single_quote == false)
+		{
+			double_quote = !double_quote;
+			index++;
+		}
+		if ((double_quote == true || single_quote == false && double_quote == false)
+			&& token->line[index] == '$')
+		{
+			while (shell->envp_copy)
+			{
+				shell->envp_copy = shell->envp_copy->next;
+			}
+		}
+		if (single_quote == true)
+			replacement[copy_index++] = token->line[index];
+
+		index++;
+	}
+	replacement[copy_index] = '\0';
+}
+
+void	find_dollar(t_shell *shell)
+{   
+	t_token *temp;
+
+	temp = shell->token_pointer;
+	while (temp != NULL)
+	{
+		if (ft_strchr(temp->line, '$') != NULL)
+			replace_dollar(shell, temp);
+		temp = temp->next;
+	}
+}
+
 	// int	index;
 	// int	copy_index;
 	// char *words_to_copy;
-	// int single_quote;
+	// int	single_quote;
 	// int	double_quote;
 
 	// single_quote = false;
@@ -49,4 +103,3 @@ void	expander(t_shell *shell)
 	// ft_strlcat(combination, words_to_copy, ft_strlen(words_to_copy));
 	// free(words_to_copy);
 	// return (combination);
-}
