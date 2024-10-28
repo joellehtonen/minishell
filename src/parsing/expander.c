c7815c5d15f6befdfdc6b302c3100215f8dffc4f
Joel Lehtonen <jlehtone@c1r5p17.hive.fi>
2024-10-28 17:34:50 +0200

    working on expander

--- src/parsing/expander.c
+++ src/parsing/expander.c
@@ -12,7 +12,6 @@ static char *expand_variable(t_shell *shell, char *pointer)
 	if (!expansion)
 		error_printer(shell, MALLOC_FAIL, true);
 	ft_strlcpy(expansion, pointer, len + 1);
-	//printf("value len is %d\n", len);
 	return (expansion);
 }
 
@@ -36,22 +35,6 @@ static char *find_variable(t_shell *shell, char *key, int len)
 	return (value);
 }
 
-static char	*handle_dollar(t_shell *shell, t_token *token, int index, int len)
-{
-	char	*key;
-	char	*value_pointer;
-	char	*expansion;
-
-	key = ft_substr(token->line, index + 1, len);
-	value_pointer = find_variable(shell, key, len);
-	free(key);
-	if (!value_pointer)
-		return (ft_strdup(""));
-	expansion = expand_variable(shell, value_pointer);
-	free(value_pointer);
-	return (expansion);
-}
-
 static int	calculate_key_len(t_token *token, int index)
 {
 	int	key_len;
@@ -66,18 +49,58 @@ static int	calculate_key_len(t_token *token, int index)
 	return (key_len);
 }
 
-static int handle_single_quote(t_token *token, int index)
+static char	*handle_dollar(t_shell *shell, t_token *token, int index, int key_len)
+{
+	char	*key;
+	char	*value_pointer;
+	char	*expansion;
+
+	key = ft_substr(token->line, index + 1, key_len);
+	value_pointer = find_variable(shell, key, key_len);
+	free(key);
+	if (!value_pointer)
+		return (ft_strdup(""));
+	expansion = expand_variable(shell, value_pointer);
+	free(value_pointer);
+	return (expansion);
+}
+
+static int expand_dollar(t_shell *shell, t_token *token, char *replacement, int index, int copy_index)
+{
+	int		key_len;
+	char	*expansion;
+
+	if (isquote(token->line[index + 1]) == true 
+		|| token->single_quote == true
+		|| token->line[index + 1] != '\0')
+		return (index);
+	key_len = calculate_key_len(token, index + 1);
+	expansion = handle_dollar(shell, token, index, key_len);
+	ft_strlcat(replacement, expansion, ft_strlen(replacement) + ft_strlen(expansion) + 1);
+	copy_index += ft_strlen(expansion);
+	index += key_len + 1;
+	free(expansion);
+	return (index);
+}
+
+// static int calc_key_value(t_shell *shell, t_token *token, int index)
+// {
+// 	int		key_len;
+
+// 	if (isquote(token->line[index + 1]) == true 
+// 		|| token->single_quote == true
+// 		|| token->line[index + 1] != '\0')
+// 		return (index);
+// 	key_len = calculate_key_len(token, index + 1);
+// }
+
+static int handle_quotes(t_token *token, int index)
 {
 	if (token->line[index] == '\'' && token->double_quote == false)
 	{
 		token->single_quote = !token->single_quote;
 		index++;
 	}
-	return (index);
-}
-
-static int handle_double_quote(t_token *token, int index)
-{
 	if (token->line[index] == '\"' && token->single_quote == false)
 	{
 		token->double_quote = !token->double_quote;
@@ -86,38 +109,35 @@ static int handle_double_quote(t_token *token, int index)
 	return (index);
 }
 
-static void	check_content(t_shell *shell, t_token *token)
-{	
-	int		index;
-	int		copy_index;
-	int		key_len;
-	char	*replacement;
-	char	*expansion;
+static char* init_replacement(t_shell *shell, t_token *token)
+{
+	char *replacement;
 
-	index = 0;
-	copy_index = 0;
-	key_len = 0;
 	token->double_quote = false;
 	token->single_quote = false;
 	replacement = malloc(sizeof(char) * (ft_strlen(token->line) + 1));
 	if (!replacement)
 		error_printer(shell, MALLOC_FAIL, true);
 	ft_memset(replacement, 0, ft_strlen(replacement));
+	return (replacement);
+}
+
+static void	check_content(t_shell *shell, t_token *token)
+{	
+	int		index;
+	int		copy_index;
+	char	*replacement;
+
+	index = 0;
+	copy_index = 0;
+	replacement = init_replacement(shell, token);
 	while (token->line[index] != '\0')
 	{
-		index = handle_single_quote(token, index);
-		index =	handle_double_quote(token, index);
-		if (token->line[index] == '$' \
-			&& isquote(token->line[index + 1]) == false \
-			&& token->line[index + 1] != '\0' \
-			&& token->single_quote == false)
+		index = handle_quotes(token, index);
+		if (token->line[index] == '$')
 		{
-			key_len = calculate_key_len(token, index + 1);
-			expansion = handle_dollar(shell, token, index, key_len);
-			ft_strlcat(replacement, expansion, ft_strlen(replacement) + ft_strlen(expansion) + 1);
-			index += key_len + 1;
-			copy_index += ft_strlen(expansion);
-			free(expansion);
+			copy_index = calc_key_value();
+			index = expand_dollar(shell, token, replacement, &index, &copy_index);
 		}
 		else if (token->line[index] == '$' && isquote(token->line[index + 1]) == true)
 			index++;
