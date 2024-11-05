c8914bd6ad1c1f292f4e8c09cdc1e3389ebe0d6d
aklimchu <aklimchu@student.hive.fi>
2024-11-05 10:24:22 +0200

    added new freeing function

--- src/parsing/read_input.c
+++ src/parsing/read_input.c
@@ -19,17 +19,14 @@ static void	null_signal(t_shell *shell)
 	free_and_exit(shell, 0);
 }
 
-static char	*create_prompt(t_shell *shell)
+static void	create_prompt(t_shell *shell)
 {
-	char *prompt;
-
-	free(shell->pwd);
+	free_str(&shell->pwd);
 	shell->pwd = get_pwd(shell->home, shell);
 	shell->envp_str = NULL;
-	prompt = ft_strjoin_four(shell->uname, ":", shell->pwd, "$ ");
-	if (prompt == NULL)
+	shell->prompt = ft_strjoin_four(shell->uname, ":", shell->pwd, "$ ");
+	if (shell->prompt == NULL)
 		error_printer(shell, MALLOC_FAIL, true);
-	return (prompt);
 }
 
 int read_input(t_shell *shell)
@@ -37,7 +34,7 @@ int read_input(t_shell *shell)
 	shell->pwd = get_pwd(shell->home, shell);
 	while (true) 
 	{
-		prompt = create_prompt(shell);
+		create_prompt(shell);
 		set_up_signals(shell);
 		shell->user_input = readline(shell->prompt);
 		if (shell->user_input == NULL)
@@ -45,11 +42,7 @@ int read_input(t_shell *shell)
 		if (input_error_check(shell) == SUCCESS)
 			handle_input(shell);
 		add_history(shell->user_input);
-		free(shell->user_input); // is it correct place?
-		free(prompt);
-		delete_all_tokens(&shell->token_pointer);
+		free_shell(&shell, false);
 	}
-	//free(shell->user_input); // replace with custom free function?
-	//printf("exit code: %d\n", shell->exit_code);
 	return (shell->exit_code);
 }
