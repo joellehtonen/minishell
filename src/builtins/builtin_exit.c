014be18fd2be77fc2395b6a447244b9acaaf8c77
aklimchu <aklimchu@student.hive.fi>
2024-11-20 10:43:32 +0200

    updated exit builtin

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -3,40 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlehtone <jlehtone@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: aklimchu <aklimchu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 12:52:56 by jlehtone          #+#    #+#             */
-/*   Updated: 2024/11/19 10:22:27 by jlehtone         ###   ########.fr       */
+/*   Updated: 2024/11/20 09:40:36 by aklimchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 static void	exit_loop(t_shell *shell, t_token *token, int i);
+static int	find_exit_code(char *str, t_shell *shell);
+static long	ft_atoi_long(const char *str);
+static const char	*white_spaces(const char *str);
 
 // The function executes "exit" builtin
 // allowing one + or - ahead of the digit
 // numbers bigger than 255 get modulo
 int	exit_exec(t_shell *shell, t_token *token)
 {
-	int	i;
-	int	exit_code;
+	int		i;
 
 	i = 0;
-	exit_code = 0;
 	if (token->next)
 	{
-		if (token->next->next && token->next->next->type != PIPE)
-			error_printer(shell, "Exit: ", TOO_MANY_ARGS, true);
-		if (token->next->line[i] == '\0')
-			error_printer(shell, "Exit: ", NUMERIC_ERROR, true);
 		exit_loop(shell, token, i);
-		exit_code = ft_atoi(token->next->line);
-		if (exit_code > 255)
-			exit_code = exit_code % 256;
-		shell->exit_code = exit_code;
+		if (token->next->next && token->next->next->type != PIPE)
+		{
+			printf("exit\n");
+			error_printer(shell, "Exit: ", TOO_MANY_ARGS, false);
+			return (1);
+		}
+		if (token->next->line[i] == '\0')
+		{
+			printf("exit\n");
+			error_printer(shell, "Exit: ", NUMERIC_ERROR, true);
+		}
+		shell->exit_code = find_exit_code(token->next->line, shell);		
 	}
-	free_and_exit(shell, exit_code);
+	printf("exit\n");
+	free_and_exit(shell, 0);
 	return (0);
 }
 
@@ -50,7 +56,10 @@ static void	exit_loop(t_shell *shell, t_token *token, int i)
 		if (token->next->line[i] == '+' || token->next->line[i] == '-')
 		{
 			if (sign == true)
+			{
+				printf("exit\n");
 				error_printer(shell, "Exit: ", NUMERIC_ERROR, true);
+			}
 			if (sign == false)
 			{
 				sign = true;
@@ -58,7 +67,66 @@ static void	exit_loop(t_shell *shell, t_token *token, int i)
 			}
 		}
 		if (ft_isdigit(token->next->line[i]) == false)
+		{
+			printf("exit\n");
 			error_printer(shell, "Exit: ", NUMERIC_ERROR, true);
+		}
 		i++;
 	}
+}
+
+static int	find_exit_code(char *str, t_shell *shell)
+{
+	long	exit_code_long;
+	int		exit_code;
+
+	exit_code = 0;
+	exit_code_long = ft_atoi_long(str);
+	if ((exit_code_long < 0 && str[0] != '-') || \
+		(exit_code_long == 0 && str[0] == '-'))
+	{
+		printf("exit\n");
+		error_printer(shell, "Exit: ", NUMERIC_ERROR, true);
+	}
+	else if (exit_code_long > 255 || exit_code_long < 0)
+		exit_code = exit_code_long % 256;
+	else
+		exit_code = exit_code_long;
+	return (exit_code);
+}
+
+static long	ft_atoi_long(const char *str)
+{
+	long int	res;
+	int			neg;
+
+	res = 0;
+	neg = 1;
+	str = white_spaces(str);
+	if (*str == 45)
+	{
+		neg = neg * (-1);
+		str++;
+	}
+	while (*str > 47 && *str < 58)
+	{
+		res = res * 10 + (*str - '0');
+		if (res < 0 && neg == 1)
+			return (-1);
+		if (res < 0 && neg == -1)
+			return (0);
+		str++;
+	}
+	return (res * neg);
+}
+
+static const char	*white_spaces(const char *str)
+{
+	while ((*str == 32) || (*str > 8 && *str < 14))
+		str++;
+	if (*str == 43 && *(str + 1) == 45)
+		return (str);
+	else if (*str == 43)
+		str++;
+	return (str);
 }
